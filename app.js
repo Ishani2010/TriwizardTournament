bc8e8e736e1982ca7d7f5115f197c10dba363aab
Ishani Sengupta <83572032+Ishani2010@users.noreply.github.com>
2021-06-15 18:41:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const homeButton = document.getElementById ("triwizard-tournament");
+homeButton.onclick=()=>{
+    location.href = "./main.html";
+}; 
+const task1Button = document.getElementById ("taskbutton1");
+task1Button.onclick=()=>{
+    location.href = "./task1.html";
+}; 
+const task2Button = document.getElementById ("taskbutton2");
+task2Button.onclick=()=>{
+    location.href = "./task2.html";
+}; 
+const task3Button = document.getElementById ("taskbutton3");
+task3Button.onclick=()=>{
+    location.href = "./task3.html";
+}; 
+const task4Button = document.getElementById ("taskbutton4");
+task4Button.onclick=()=>{
+    location.href = "./task4.html";
+}; 
